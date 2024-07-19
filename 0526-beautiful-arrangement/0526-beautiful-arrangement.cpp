101434d86e0cf78e759beb41e3fb2dbb77103433
aryankhatuwala <86549258+aryankhatuwala@users.noreply.github.com>
2024-07-19 19:55:08 +0530

    Time: 60 ms (35.53%), Space: 7.4 MB (54.91%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int func(int place, int n,vector<int>&used)
+    int func(int place, int n, int mask)
     {
         if(place==n+1) return 1;
+        // if(mask==(1<<(n+1)-1)) return 1;
 
         int inc=0;
         for(int k=1;k<=n;k++)
         {
-            if((k%place==0 or place%k==0) and used[k]!=1)
+            if((k%place==0 or place%k==0) and (mask&(1<<k))==0)
             {
-            used[k]=1;
-            inc+=func(place+1,n,used);
-            used[k]=0;
+             int new_mask=(1<<k)|mask;
+             inc+=func(place+1,n,new_mask);
             }
         }
         return inc;
     }
     int countArrangement(int n) {
-        vector<int>used(n+1,0);
-        return func(1,n,used);
+        // vector<int>used(n+1,0);
+        return func(1,n,0);
     }
 };
