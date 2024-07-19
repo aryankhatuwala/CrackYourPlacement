08e2f4b5e993a3311c75b432d8bcb06d44c6e90a
aryankhatuwala <86549258+aryankhatuwala@users.noreply.github.com>
2024-07-19 12:49:10 +0530

    Time: 307 ms (13.46%), Space: 50.7 MB (44.26%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    bool func(int pos, int k, vector<int>& stones, int n, set<int>& st, map<pair<int, int>, bool>& memo) {
-        if (pos == stones[n - 1]) return true;
-        if (pos > stones[n - 1] || k <= 0) return false;
+    bool func(int pos,int k, vector<int>& stones, int n,set<int>&st,
+     map<pair<int,int>,bool>&mp )
+    {
+        if(pos==stones[n-1]) return true;
+        if(pos>stones[n-1] || k<=0 || pos<0) return false;
 
-        if (st.find(pos) == st.end()) return false;
+        if(mp.find({pos,k})!=mp.end())return mp[{pos,k}];
 
-        auto key = make_pair(pos, k);
-        if (memo.find(key) != memo.end()) return memo[key];
+        if(st.find(pos)==st.end()) return false;
 
-        bool one = func(pos + k, k, stones, n, st, memo);
-        bool two = func(pos + k - 1, k - 1, stones, n, st, memo);
-        bool three = func(pos + k + 1, k + 1, stones, n, st, memo);
-
-        memo[key] = one || two || three;
-        return memo[key];
+        bool one,two,three;
+        one=two=three=false;
+        one=func(pos+ k,k,stones,n,st,mp);
+        two=func(pos+ k-1,k-1,stones,n,st,mp);
+        three=func(pos+ k+1,k+1,stones,n,st,mp);
+        
+        return  mp[{pos,k}]= one||two||three;
     }
-
     bool canCross(vector<int>& stones) {
-        int n = stones.size();
-        if(stones[1]!=1) return false;
-        set<int> st(stones.begin(), stones.end());
-        map<pair<int, int>, bool> memo;
-        return func(1, 1, stones, n, st, memo); // Start with k=1 instead of k=0
+        int n=stones.size();
+        set<int>st(stones.begin(),stones.end());
+        map<pair<int,int>,bool>mp;
+        if(stones[1]!=1) return 0;
+        return func(1,1,stones,n,st,mp);
     }
 };
