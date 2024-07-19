56c81d64622f49d6c7df300fbf561c8b4335ee06
aryankhatuwala <86549258+aryankhatuwala@users.noreply.github.com>
2024-07-19 20:06:25 +0530

    Time: 23 ms (79.6%), Space: 13 MB (28.23%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
-    int func(int place, int n, int mask)
+    int func(int place, int n, int mask,map<pair<int,int>,int>&mp)
     {
         if(place==n+1) return 1;
+        if(mp.find({place,mask})!=mp.end()) return mp[{place,mask}];
 
         int inc=0;
         for(int k=n;k>=1;k--)
@@ -10,13 +11,15 @@ public:
             if((k%place==0 or place%k==0) and (mask&(1<<k))==0)
             {
              int new_mask=(1<<k)|mask;
-             inc+=func(place+1,n,new_mask);
+             inc+=func(place+1,n,new_mask,mp);
             }
         }
-        return inc;
+        return mp[{place,mask}]=inc;
     }
     int countArrangement(int n) {
         // vector<int>used(n+1,0);
-        return func(1,n,0);
+        // vector<vector<int>>dp(n+1,-1);
+        map<pair<int,int>,int>mp;
+        return func(1,n,0,mp);
     }
 };
