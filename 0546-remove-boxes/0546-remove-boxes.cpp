a8b1a1b32d40d22fe2f7f4c9e16c314fcd63a9d4
aryankhatuwala <86549258+aryankhatuwala@users.noreply.github.com>
2024-07-21 11:10:47 +0530

    Time: 109 ms (48.35%), Space: 13.3 MB (98.96%) - LeetHub

--- 0546-remove-boxes/0546-remove-boxes.cpp
+++ 0546-remove-boxes/0546-remove-boxes.cpp
@@ -1,29 +1,23 @@
+int dp[101][101][101];
 class Solution {
-    vector<vector<vector<int>>> dp;
 public:
-    int removeBoxes(vector<int>& boxes) {
-        int n = boxes.size();
-        dp.resize(n+1, vector<vector<int>>(n+1, vector<int>(n+1, -1)));
-        
-        return solve(boxes, 0, n-1, 0);
-    }
-    
-    int solve(vector<int> &boxes, int i, int j, int cnt) {
-        if(i > j) return 0;
-        
-        if(dp[i][j][cnt] != -1) return dp[i][j][cnt];
-        
-        int i0 = i, cnt0 = cnt;
-        
-        while(i + 1 <= j && boxes[i] == boxes[i+1]) {i++; cnt++;}
-        
-        int ans = (cnt+1) * (cnt+1) + solve(boxes, i+1, j, 0);
-        
-        for(int k=i+1; k<=j; k++) {
-            if(boxes[k] == boxes[i]) {
-                ans = max(ans, solve(boxes, i+1, k-1, 0) + solve(boxes, k, j, cnt+1));
-            }
+    int rec(int l,int r,int cnt,vector<int>& boxes){
+        if(l>r)return 0;
+
+        if(dp[l][r][cnt]!=-1) return dp[l][r][cnt];
+
+        int ans=(cnt+1)*(cnt+1) + rec(l+1,r,0,boxes);
+
+        for(int m=l+1;m<=r;m++)
+        {
+            if(boxes[m]==boxes[l])
+                ans=max(ans,rec(l+1,m-1,0,boxes)+rec(m,r,cnt+1,boxes));
+            
         }
-        return dp[i0][j][cnt0] = ans;
+        return dp[l][r][cnt]=ans;
+    }
+    int removeBoxes(vector<int>& boxes) {
+        memset(dp,-1,sizeof(dp));
+        return rec(0,boxes.size()-1,0,boxes);
     }
 };
