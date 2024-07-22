9a5ddd194900e21e179249b74843c7fe89a42a79
aryankhatuwala <86549258+aryankhatuwala@users.noreply.github.com>
2024-07-22 23:38:05 +0530

    Time: 23 ms (71.39%), Space: 33.8 MB (16.68%) - LeetHub

---
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* func(TreeNode* root1, TreeNode* root2)
+    {
+        if(!root1 and root2) return root2;
+        if(!root2 and root1) return root1;
+        if(!root1 and !root2) return nullptr;
+
+        int sum=root1->val+root2->val;
+        TreeNode* temp=new TreeNode(sum);
+
+        temp->left=func(root1->left, root2->left);
+        temp->right=func(root1->right, root2->right);
+
+        return temp;
+    }
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        return func(root1, root2);
+    }
+};
