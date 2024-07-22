6088ab1609bc0ce0fe2d5b5c1d2d5b2ae5732ee3
aryankhatuwala <86549258+aryankhatuwala@users.noreply.github.com>
2024-07-22 18:59:20 +0530

    Time: 0 ms (100%), Space: 12.1 MB (7.38%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -1,28 +1,24 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    TreeNode* func(TreeNode* root)
-    {
-        if(!root) return nullptr;
-        
-        auto lefti=func(root->left);
-        auto righti=func(root->right);
+    TreeNode* invertTree(TreeNode* root) {
+        if (!root) {
+            return NULL;
+        }
+        queue<TreeNode*> todo;
+        todo.push(root);
+        while (!todo.empty()) {
+            TreeNode* node = todo.front();
+            todo.pop();
 
-        root->left=righti;
-        root->right=lefti;
+            swap(node -> left, node -> right);
+            
+            if (node -> left) 
+                todo.push(node -> left);
+            
+            if (node -> right) 
+                todo.push(node -> right);
+            
+        }
         return root;
     }
-    TreeNode* invertTree(TreeNode* root) {
-        return func(root);
-    } 
 };
