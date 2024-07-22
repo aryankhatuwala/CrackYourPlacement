1aa6cf8562d923bbd9351bc35937f98f7ebc728b
aryankhatuwala <86549258+aryankhatuwala@users.noreply.github.com>
2024-07-22 11:58:44 +0530

    Time: 0 ms (100%), Space: 7.8 MB (31.88%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        
+        int n=haystack.size(), m=needle.size();
+        int h_hash,n_hash; 
+        h_hash=n_hash=0;
+        int mod=101;
+        int d=10;
+        for(int i=0;i<m;i++)
+        {
+            h_hash=(h_hash*d+haystack[i]-'a'+1)%mod;
+            n_hash=(n_hash*d+needle[i]-'a'+1)%mod;
+        }
+        cout<<n_hash<<" ";
+        int q=1;
+        for(int i=0;i<m-1;i++) q=(q*d)%mod;
+
+        for(int i=0;i<= n-m;i++)
+        {
+           if(h_hash==n_hash)
+           {
+              int flag=true; 
+              for(int j=0;j<m;j++)
+              {
+                if(haystack[i+j]!=needle[j]) flag=false;
+              }
+              if(flag) return i;
+           }
+           else if(i<n-m)
+           {
+             h_hash=((h_hash-(haystack[i]-'a'+1)*q)*d + haystack[i+m]-'a'+1)%mod;
+             if(h_hash<0)
+             h_hash+=mod;
+            cout<<h_hash<<" ";
+           }
+        }
+        return -1;
+    }
+};
