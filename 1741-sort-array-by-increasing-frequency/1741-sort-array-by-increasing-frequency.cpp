f691932cb36879c0457183269078c4380964a478
aryankhatuwala <86549258+aryankhatuwala@users.noreply.github.com>
2024-07-23 09:52:38 +0530

    Time: 3 ms (89.91%), Space: 14.9 MB (55.33%) - LeetHub

---
+++ 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool static cmp(pair<int,int>&a, pair<int,int>&b)
+    {
+        return (a.second==b.second)? a.first>b.first: 
+        a.second<b.second;
+    }
+    vector<int> frequencySort(vector<int>& nums) {
+        map<int,int>mp;
+        for(auto it: nums) mp[it]++;
+
+        vector<pair<int,int>>arr;
+        for(auto it:mp)
+        arr.push_back(it);
+
+        sort(arr.begin(),arr.end(),cmp);
+
+        vector<int>ans;
+        for(auto it:arr)
+        {
+            for(int i=0;i<it.second;i++)
+            ans.push_back(it.first);
+        }
+        return ans;
+    }
+};
