85e1c4c88118c07504fe008f506d025e9033effc
aryankhatuwala <86549258+aryankhatuwala@users.noreply.github.com>
2024-07-27 10:42:32 +0530

    Time: 143 ms (92.93%), Space: 96.8 MB (76.59%) - LeetHub

---
+++ 3235-minimum-cost-to-convert-string-i/3235-minimum-cost-to-convert-string-i.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
+        
+        int n=original.size();
+        vector<vector<int>>mat(26,vector<int>(26,INT_MAX));
+
+        for(int i=0;i<26;i++) mat[i][i]=0;
+
+        for(int i=0;i<n;i++)
+        {
+            mat[original[i]-'a'][changed[i]-'a']=
+            min( mat[original[i]-'a'][changed[i]-'a'], cost[i]);
+        }
+
+        //floyd warshall
+        for(int k=0;k<26;k++)
+        {
+            for(int i=0;i<26;i++)
+            {
+                for(int j=0;j<26;j++)
+                {
+                    if(mat[i][k]!=INT_MAX and mat[k][j]!=INT_MAX)
+                    mat[i][j]=min(mat[i][j],mat[i][k]+mat[k][j]);
+                }
+            }
+        }
+
+        //main logic
+        long long ans=0;
+        for(int i=0;i<source.size();i++)
+        {
+            if(source[i]==target[i]) continue;
+            
+            int dist=mat[source[i]-'a'][target[i]-'a'];
+
+            if(dist==INT_MAX) return -1;
+
+            ans+= dist;
+        }
+        return ans;
+    }
+};
