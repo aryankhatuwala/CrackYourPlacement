3e76255be83ca639adcb9fa8e8763c789ec53b13
aryankhatuwala <86549258+aryankhatuwala@users.noreply.github.com>
2024-07-27 10:48:00 +0530

    Time: 304 ms (21.34%), Space: 204.4 MB (7.92%) - LeetHub

--- 3235-minimum-cost-to-convert-string-i/3235-minimum-cost-to-convert-string-i.cpp
+++ 3235-minimum-cost-to-convert-string-i/3235-minimum-cost-to-convert-string-i.cpp
@@ -1,43 +1,56 @@
 class Solution {
 public:
-    long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
-        
-        int n=original.size();
-        vector<vector<int>>mat(26,vector<int>(26,INT_MAX));
+    void bfs(unordered_map<char,vector<pair<char,int>>>&graph,char source,vector<vector<int>>&distances){
+//Normal BFS shortest path code
+//Saving calculated distance in distances array
 
-        for(int i=0;i<26;i++) mat[i][i]=0;
+        queue<pair<int,int>>q;
+        q.push({source,0});
+        while(!q.empty()){
+            int node = q.front().first;
+            int distance=q.front().second;
+            q.pop();
+            for(auto child:graph[node]){
+                if(distances[source-'a'][child.first-'a']>distance+child.second){
 
-        for(int i=0;i<n;i++)
-        {
-            mat[original[i]-'a'][changed[i]-'a']= 
-            min( mat[original[i]-'a'][changed[i]-'a'], cost[i]);
-        }
-
-        //floyd warshall
-        for(int k=0;k<26;k++)
-        {
-            for(int i=0;i<26;i++)
-            {
-                for(int j=0;j<26;j++)
-                {
-                    if(mat[i][k]!=INT_MAX and mat[k][j]!=INT_MAX)
-                    mat[i][j]=min(mat[i][j],mat[i][k]+mat[k][j]);
+                    distances[source-'a'][child.first-'a']=distance+child.second;
+                    q.push({child.first,distance+child.second});
                 }
             }
-        }
-
-        //main logic
-        long long ans=0;
-        for(int i=0;i<source.size();i++)
-        {
-            if(source[i]==target[i]) continue;
             
-            int dist=mat[source[i]-'a'][target[i]-'a'];
-
-            if(dist==INT_MAX) return -1;
-
-            ans+= dist;
         }
+        return;
+    }
+    
+    long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
+
+        unordered_map<char,vector<pair<char,int>>>graph;
+
+//Initialzing the graph in the node schema : {parent,{child,cost}}
+        for(int i=0;i<original.size();i++){
+            graph[original[i]].push_back({changed[i],cost[i]});
+        }
+
+        //Distance array required will only need 26*26 space.
+        vector<vector<int>>distances(26,vector<int>(26,INT_MAX));
+
+        //Running BFS from every node of original string.
+        for(auto it:original)
+        bfs(graph,it,distances);
+        
+        long long ans=0;
+        for(int i=0;i<source.size();i++){
+
+//No need to add anything to answer if source and target are same
+            if(source[i]==target[i])continue;
+
+//If Distance is infinite, the target is not achievable and hence return -1
+            if(distances[source[i]-'a'][target[i]-'a']==INT_MAX)return -1;
+
+//Otherwise add the corresponding value in the distances array
+            else ans+=distances[source[i]-'a'][target[i]-'a'];
+        }
+        
         return ans;
     }
 };
