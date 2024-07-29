0111e4ab6237b6c85c8dbcd2b0ffdd319c76dcae
aryankhatuwala <86549258+aryankhatuwala@users.noreply.github.com>
2024-07-29 12:47:47 +0530

    Time: 2638 ms (5.01%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,13 +1,7 @@
-SELECT  IFNULL(
-        (SELECT DISTINCT salary 
-         FROM employee 
-         ORDER BY salary DESC 
-         LIMIT 1 OFFSET 1), 
-        NULL
-    ) AS SecondHighestSalary;
-
-
-
-
-
-
+SELECT IFNULL(
+    (SELECT distinct salary 
+     FROM employee 
+     ORDER BY salary DESC 
+     LIMIT 1 OFFSET 1), 
+    NULL
+) AS SecondHighestSalary;
