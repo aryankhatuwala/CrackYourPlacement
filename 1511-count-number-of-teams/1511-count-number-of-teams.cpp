8cb898cc5600eedee967521bf62e4d3eda6d4f06
aryankhatuwala <86549258+aryankhatuwala@users.noreply.github.com>
2024-07-29 11:10:47 +0530

    Time: 39 ms (54.16%), Space: 12.1 MB (30.87%) - LeetHub

---
+++ 1511-count-number-of-teams/1511-count-number-of-teams.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int numTeams(vector<int>& rating) {
+        int n = rating.size();
+        if (n < 3) return 0;
+
+        vector<int> increasing(n, 0), decreasing(n, 0);
+
+        // Fill increasing array
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (rating[j] < rating[i]) {
+                    increasing[i]++;
+                }
+            }
+        }
+
+        // Fill decreasing array
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (rating[j] > rating[i]) {
+                    decreasing[i]++;
+                }
+            }
+        }
+
+        int teams = 0;
+
+        // Count valid teams
+        for (int i = 1; i < n - 1; ++i) {
+            int inc = 0, dec = 0;
+            for (int j = i + 1; j < n; ++j) {
+                if (rating[i] < rating[j]) {
+                    inc++;
+                }
+                if (rating[i] > rating[j]) {
+                    dec++;
+                }
+            }
+            teams += increasing[i] * inc + decreasing[i] * dec;
+        }
+
+        return teams;
+    }
+};
