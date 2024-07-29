9cb7ec7d61743c8d03cbc83b3b1e7063fafc698c
aryankhatuwala <86549258+aryankhatuwala@users.noreply.github.com>
2024-07-29 11:54:21 +0530

    Time: 50 ms (47.52%), Space: 11.8 MB (35.83%) - LeetHub

--- 1511-count-number-of-teams/1511-count-number-of-teams.cpp
+++ 1511-count-number-of-teams/1511-count-number-of-teams.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
-    int dp[1002][1002][4];
-    int func(int cur, int prev, int count, vector<int>&rating,int n)
-    {
-        if(count==3) return 1;
-        if(cur==n) return 0;
-        if(dp[cur][prev+1][count]!=-1) return dp[cur][prev+1][count];
-        
-        int inc, notinc;
-        inc=0;
-        notinc=func(cur+1,prev, count, rating,n);
-        if(prev==-1 || rating[cur]>rating[prev])
-        {
-            inc=func(cur+1,cur,count+1,rating,n);
-        }
-        return dp[cur][prev+1][count]=inc+notinc;
-    }
     int numTeams(vector<int>& rating) {
-        memset(dp,-1,sizeof(dp));
         int n=rating.size();
-
-        int ans1=func(0,-1,0,rating,n);
-        reverse(rating.begin(),rating.end());
-        memset(dp,-1,sizeof(dp));
-        int ans2=func(0,-1,0,rating,n);
-        return ans1+ans2;
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            int leftSmaller=0, rightGreater=0;
+            int leftGreater=0, rightSmaller=0;
+            for(int j=0;j<n;j++)
+            {
+                if(j<i and rating[j]<rating[i]) leftSmaller++;
+                if(j>i and rating[j]>rating[i]) rightGreater++;
+                if(j<i and rating[j]>rating[i]) leftGreater++;
+                if(j>i and rating[j]<rating[i]) rightSmaller++;
+            }
+            ans+=leftSmaller*rightGreater;
+            ans+=leftGreater*rightSmaller;
+        }
+        return ans;
     }
 };
