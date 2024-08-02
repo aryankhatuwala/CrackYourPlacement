554b3ca79e6db40b8fa0d2220e5122082c31bc36
aryankhatuwala <86549258+aryankhatuwala@users.noreply.github.com>
2024-08-02 12:49:32 +0530

    Time: 81 ms (14.15%), Space: 83.1 MB (47.74%) - LeetHub

--- 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -8,14 +8,12 @@ public:
         for(int i=0;i<n;i++) if(nums[i]==1) ones++;
         
         int bad=0;
-        for(int i=0;i<ones;i++)
+        for(int i=0;i<ones;i++) if(nums[i]==0) bad++;
+
+        int ans=bad;
+        for(int i=ones;i<n+n;i++)
         {
-           if(nums[i]==0) bad++;
-        }
-        int ans=bad, k=ones;
-        for(int i=k;i<n+n;i++)
-        {
-           if(nums[(i-k)%n]==0) bad--;
+           if(nums[(i-ones)%n]==0) bad--;
            if(nums[i%n]==0) bad++;
            ans=min(ans, bad);
         }
