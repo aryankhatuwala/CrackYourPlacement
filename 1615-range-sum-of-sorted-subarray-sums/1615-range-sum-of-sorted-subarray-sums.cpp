257c737eae792d6c78740c719fc62fd4590d2ce4
aryankhatuwala <86549258+aryankhatuwala@users.noreply.github.com>
2024-08-04 19:08:38 +0530

    Time: 86 ms (74.83%), Space: 13.5 MB (75.95%) - LeetHub

--- 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int rangeSum(vector<int>& nums, int n, int left, int right) {
-	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> mqueue;
-	for (int i=0; i<n; i++)
-		mqueue.push({nums[i], i+1});
-
-	int ans = 0, mod = 1e9+7;
-	for (int i=1; i<=right; i++) {
-		auto p = mqueue.top();
-		mqueue.pop();
-		if (i >= left)
-			ans = (ans + p.first) % mod;
-		if (p.second < n) {
-			p.first += nums[p.second++];
-			mqueue.push(p);
-		}
-	}
-	return ans;
-}
+        int arr[n * (n + 1) / 2];
+        for (int i = 0, k = 0; i < n; ++i) {
+            int s = 0;
+            for (int j = i; j < n; ++j) {
+                s += nums[j];
+                arr[k++] = s;
+            }
+        }
+        sort(arr, arr + n * (n + 1) / 2);
+        int ans = 0;
+        const int mod = 1e9 + 7;
+        for (int i = left - 1; i < right; ++i) {
+            ans = (ans + arr[i]) % mod;
+        }
+        return ans;
+    }
 };
