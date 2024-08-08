bf60be8f61b9ee2ca1e15fd32ce2c14fe8543a0c
aryankhatuwala <86549258+aryankhatuwala@users.noreply.github.com>
2024-08-08 15:38:39 +0530

    Time: 10 ms (51.13%), Space: 13.9 MB (12.09%) - LeetHub

--- 0921-spiral-matrix-iii/0921-spiral-matrix-iii.cpp
+++ 0921-spiral-matrix-iii/0921-spiral-matrix-iii.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+        vector<vector<int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; // East, South, West, North
+        vector<vector<int>> result = {{rStart, cStart}};
+        int steps = 0, d = 0;
         
-        vector<vector<int>> ans;
-        ans.push_back({rStart, cStart}); //Pushing the starting point in answer
-        int topRow = rStart-1; //Row above starting point
-        int bottomRow = rStart+1; //Row below starting point
-        int leftCol = cStart-1; //Col left to starting point
-        int rightCol = cStart+1; //Col right to starting point
-        
-        while(topRow != -1 || bottomRow != rows || leftCol != -1 || rightCol != cols){ //Untill all rows and columns are exhausted
+        while (result.size() < rows * cols) {
+            if (d == 0 || d == 2) steps++;
             
-            if(rightCol != cols){ //Checking if this col is exhausted
-                for(int i = topRow+1; i < bottomRow;i++) ans.push_back({i, rightCol}); //Running loop from one bottom of top row till bottom row in right col
-                rightCol++; //Incrementing the col forward
-            }
-            //Similar things are done below with different cols and rows
-            if(bottomRow != rows){
-                for(int j = rightCol-1; j > leftCol; j--) ans.push_back({bottomRow, j});
-                bottomRow++;
+            for (int i = 0; i < steps; i++) {
+                rStart += directions[d][0];
+                cStart += directions[d][1];
+                
+                if (rStart >= 0 && rStart < rows && cStart >= 0 && cStart < cols) {
+                    result.push_back({rStart, cStart});
+                }
+                
+                if (result.size() == rows * cols) return result;
             }
             
-            if(leftCol != -1){
-                for(int i = bottomRow-1; i > topRow; i--) ans.push_back({i, leftCol});
-                leftCol--;
-            }
-            
-            if(topRow != -1){
-                for(int j = leftCol+1; j < rightCol; j++) ans.push_back({topRow, j});
-                topRow--;
-            }  
+            d = (d + 1) % 4;
         }
-        return ans;
+        
+        return result;
     }
 };
