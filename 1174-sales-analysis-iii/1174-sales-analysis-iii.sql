f04927e34b8c8f4d5262f2eb33a553c80dd0d187
aryankhatuwala <86549258+aryankhatuwala@users.noreply.github.com>
2024-08-12 12:07:27 +0530

    Time: 1130 ms (45.48%), Space: 0B (100%) - LeetHub

--- 1174-sales-analysis-iii/1174-sales-analysis-iii.sql
+++ 1174-sales-analysis-iii/1174-sales-analysis-iii.sql
@@ -1,7 +1,7 @@
 SELECT DISTINCT p.product_id, p.product_name
 FROM product p
 JOIN sales s ON p.product_id = s.product_id
-WHERE s.sale_date BETWEEN '2019-01-01' AND '2019-03-31'
+WHERE s.sale_date 
 AND p.product_id NOT IN (
     SELECT DISTINCT product_id
     FROM sales
