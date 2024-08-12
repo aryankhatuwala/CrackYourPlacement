dc445f93651931a493298ed2db1135116f8e741f
aryankhatuwala <86549258+aryankhatuwala@users.noreply.github.com>
2024-08-12 12:09:39 +0530

    Time: 1041 ms (64.2%), Space: 0B (100%) - LeetHub

--- 1174-sales-analysis-iii/1174-sales-analysis-iii.sql
+++ 1174-sales-analysis-iii/1174-sales-analysis-iii.sql
@@ -1,8 +1,6 @@
-# Write your MySQL query statement below
-select distinct p.product_id, p.product_name
-from product p inner join sales s 
-on p.product_id=s.product_id
-where p.product_id not in (
-    select product_id from sales
-    where sale_date not between '2019-01-01' and '2019-03-31'
-)
+# Wherever you are given a range, keep MIN() and MAX() in mind
+SELECT Product.product_id, Product.product_name FROM Product 
+JOIN Sales 
+ON Product.product_id = Sales.product_id 
+GROUP BY Sales.product_id 
+HAVING MIN(Sales.sale_date) >= "2019-01-01" AND MAX(Sales.sale_date) <= "2019-03-31";
