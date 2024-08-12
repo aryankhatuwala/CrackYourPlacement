2372517f3a58136098c661e08183ca4c7cee04f0
aryankhatuwala <86549258+aryankhatuwala@users.noreply.github.com>
2024-08-12 21:55:26 +0530

    Time: 1605 ms (11.87%), Space: 0B (100%) - LeetHub

--- 1268-market-analysis-i/1268-market-analysis-i.sql
+++ 1268-market-analysis-i/1268-market-analysis-i.sql
@@ -1,8 +1,5 @@
-# Write your MySQL query statement below
-select u.user_id as  buyer_id, 
-u.join_date as join_date,
-count(o.order_date) as orders_in_2019
-from users u left outer join orders o 
-on u.user_id=o.buyer_id and o.order_date between "2019-01-01" and "2019-12-31"
-group by u.user_id
-
+SELECT u.user_id as buyer_id, u.join_date, count(o.order_id) as 'orders_in_2019'
+FROM users u
+LEFT JOIN Orders o
+ON o.buyer_id=u.user_id AND YEAR(order_date)='2019'
+GROUP BY u.user_id
