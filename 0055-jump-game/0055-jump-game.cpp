c3ec9fc4aa48ece6d7999259ef345b0f0cdd7558
aryankhatuwala <86549258+aryankhatuwala@users.noreply.github.com>
2024-08-13 12:39:28 +0530

    Time: 42 ms (68.98%), Space: 51.2 MB (32.53%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,40 +1,15 @@
 class Solution {
 public:
-    bool func(int ind,vector<int>& nums, int n,vector<int>&dp )
-    {
-        if(ind==n-1) return true;
-        if(ind>=n) return false;
-        if(dp[ind]!=-1) return dp[ind];
-
-        bool ans=false;
-        for(int k=1;k<=nums[ind];k++)
-        {
-            if(ind+k>=n) break;
-
-            ans|= func(ind+k,nums,n,dp);
-        }
-        return dp[ind]=ans;
-    }
     bool canJump(vector<int>& nums) {
-        
-        int n=nums.size();
-        vector<int>dp(n,-1);
-        return func(0,nums,n,dp);
-
-        // vector<bool>dp2(n+1,false);
-        // dp2[n-1]=true;
-        // for(int i=n-2;i>=0;i--)
-        // {
-        //     bool ans=false;
-        //     for(int k=1;k<=nums[i];k++)
-        //     {
-        //         if(i+k<n)
-        //         ans|=dp2[i+k];
-
-        //         if(i+k>=n) break;
-        //     }
-        //     dp2[i]=ans;
-        // }
-        // return dp2[0];
+        int i, minjump = 0;
+        for(i = nums.size()-2; i >= 0; i--){
+            minjump++;
+            if(nums[i] >= minjump)
+			    minjump = 0;
+        }
+        if(minjump == 0) 
+		    return true;
+        else 
+		    return false;
     }
 };
