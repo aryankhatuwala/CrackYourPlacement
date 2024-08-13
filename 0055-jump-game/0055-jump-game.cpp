f51323ab1189aa1019c1a59ea17aebcb685fdaaa
aryankhatuwala <86549258+aryankhatuwala@users.noreply.github.com>
2024-08-13 12:34:28 +0530

    Time: 2890 ms (5.01%), Space: 53.7 MB (8.11%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool func(int ind,vector<int>& nums, int n,vector<int>&dp )
+    {
+        if(ind==n-1) return true;
+        if(ind>=n) return false;
+        if(dp[ind]!=-1) return dp[ind];
+
+        bool ans=false;
+        for(int k=1;k<=nums[ind];k++)
+        {
+            ans|= func(ind+k,nums,n,dp);
+        }
+        return dp[ind]=ans;
+    }
+    bool canJump(vector<int>& nums) {
+        
+        int n=nums.size();
+        vector<int>dp(n,-1);
+        // return func(0,nums,n,dp);
+
+        vector<bool>dp2(n+1,false);
+        dp2[n-1]=true;
+        for(int i=n-2;i>=0;i--)
+        {
+            bool ans=false;
+            for(int k=1;k<=nums[i];k++)
+            {
+                if(i+k<n)
+                ans|=dp2[i+k];
+                
+                if(i+k>=n) break;
+            }
+            dp2[i]=ans;
+        }
+        return dp2[0];
+    }
+};
