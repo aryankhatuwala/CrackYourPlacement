c409ad8202d74da3c143c0fbaeb0f79ed2539a17
aryankhatuwala <86549258+aryankhatuwala@users.noreply.github.com>
2024-08-15 16:32:26 +0530

    Time: 224 ms (32.69%), Space: 107.6 MB (28.11%) - LeetHub

--- 1622-max-value-of-equation/1622-max-value-of-equation.cpp
+++ 1622-max-value-of-equation/1622-max-value-of-equation.cpp
@@ -7,8 +7,10 @@ public:
         {
             while(pq.size() && point[0]-pq.top().second>k)
                 pq.pop();
+
             if(!pq.empty())
                 ans = max(ans, point[0] + point[1] + pq.top().first);
+                
             pq.push(make_pair(point[1]-point[0], point[0]));
         }
         return ans;
