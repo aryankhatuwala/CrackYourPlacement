4537962d5ea32724733eabaf8a375d0cf05ea798
aryankhatuwala <86549258+aryankhatuwala@users.noreply.github.com>
2024-08-16 23:09:12 +0530

    Time: 5 ms (21.52%), Space: 10.6 MB (26.94%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -1,49 +1,28 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
+        vector<int> ans;
+        ListNode* h = head;
+        int count = 1;
         
-        vector<int>ans;
-        ListNode* h=head;
-        int flag=0, count=1;
-        while(head and count !=right+1)
-        {
-            if(count==left)
-            flag=1;
-
-            if(flag==1)
-            ans.push_back(head->val);
-
-            head=head->next;
+        // Collect values from the sublist defined by 'left' to 'right'
+        while (head && count <= right) {
+            if (count >= left) {
+                ans.push_back(head->val);
+            }
+            head = head->next;
             count++;
         }
 
-        for(auto it:ans)cout<<it<<" ";
-
-        int k=ans.size()-1;
-        // cout<<k;
-        head=h;
-        flag=0;
-        count=1;
-        while(head and count !=right+1)
-        {
-            cout<<"*";
-            if(count ==left)
-            flag=1;
-
-            if(flag==1)
-            head->val=ans[k--];
-
-            head=head->next;
+        // Reverse the sublist values in the original list
+        head = h;
+        count = 1;
+        int k = ans.size() - 1;
+        while (head && count <= right) {
+            if (count >= left) {
+                head->val = ans[k--];
+            }
+            head = head->next;
             count++;
         }
 
