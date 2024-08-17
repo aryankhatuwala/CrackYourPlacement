df58af8bcc24d2d0b7c6dd1c03e72654d2a82f5b
aryankhatuwala <86549258+aryankhatuwala@users.noreply.github.com>
2024-08-17 12:05:41 +0530

    Time: 22 ms (84.15%), Space: 22.8 MB (35.09%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -1,27 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     void reorderList(ListNode* head) {
-        if ((!head) || (!head->next) || (!head->next->next)) return; // Edge cases
-        
-        stack<ListNode*> my_stack;
-        ListNode* ptr = head;
-        int size = 0;
-        while (ptr != NULL) // Put all nodes in stack
+        ListNode* dum=new ListNode(0);
+        dum->next=head;
+        ListNode* slow=dum, *fast=dum;
+
+        while(fast and fast->next)  //find middle
         {
-            my_stack.push(ptr);
-            size++;
-            ptr = ptr->next;
+            slow=slow->next;
+            fast=fast->next->next;
         }
-        
-        ListNode* pptr = head;
-        for (int j=0; j<size/2; j++) // Between every two nodes insert the one in the top of the stack
+        //reverse second half;
+        ListNode* prev=nullptr, *cur=slow->next;
+        slow->next=nullptr;
+        while(cur)
         {
-            ListNode *element = my_stack.top();
-            my_stack.pop();
-            element->next = pptr->next;
-            pptr->next = element;
-            pptr = pptr->next->next;
+            ListNode* next=cur->next;
+            cur->next=prev;
+            prev=cur;
+            cur=next;
         }
-        pptr->next = NULL;
+
+        //merge the two halves
+        ListNode* first=dum->next, *second=prev;
+        if(first)
+        cout<<first->val;
+        if(second) cout<<second->val;
+        while(first and second)
+        {
+            ListNode* sec_next=second->next;
+            ListNode* first_next=first->next;
+            second->next=first->next;
+            first->next=second;
+            first=first_next;
+            second=sec_next;
+        }
+
     }
 };
