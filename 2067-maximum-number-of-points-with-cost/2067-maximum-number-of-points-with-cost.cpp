28b15eae1f17963282172cc36c3466ba7613cfe7
aryankhatuwala <86549258+aryankhatuwala@users.noreply.github.com>
2024-08-17 23:00:45 +0530

    Time: 164 ms (88.01%), Space: 88.9 MB (77.66%) - LeetHub

--- 2067-maximum-number-of-points-with-cost/2067-maximum-number-of-points-with-cost.cpp
+++ 2067-maximum-number-of-points-with-cost/2067-maximum-number-of-points-with-cost.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     long long maxPoints(vector<vector<int>>& points) {
         int row = points.size();
-        if(row == 0) {
-            // emtpy array
+        if(row == 0) // emtpy array
             return 0;
-        }
+        
 
         int m = points[0].size();
 
@@ -15,22 +14,20 @@ public:
 
         for(int i=1; i<row; i++) 
         {
-            left.front() = dp.front();
-            for(int j=1; j<m; j++) 
-                // left[j-1]-1, the minus 1 counts the shift
+            left.front() = dp.front(); //for 0th index
+            for(int j=1; j<m; j++) // left[j-1]-1, the minus 1 counts the shift
                 left[j] = max(dp[j], left[j-1]-1); 
             
 
-            right.back() = dp.back();
+            right.back() = dp.back(); //for (m-1)th index
             for(int j=m-2; j>=0; j--) 
                 right[j] =  max(dp[j], right[j+1]-1);
             
 
-            for (int j=0; j<m; j++) 
+            for (int j=0; j<m; j++) //final ans
                 dp[j] = points[i][j] + max(left[j], right[j]);
-            
-        }
 
+        }
         return *max_element(dp.begin(), dp.end());
     }
 };
