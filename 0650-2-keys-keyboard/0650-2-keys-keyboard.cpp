db90a3ea55442a4ffed28cc99cbfeb775a693f57
aryankhatuwala <86549258+aryankhatuwala@users.noreply.github.com>
2024-08-19 14:33:24 +0530

    Time: 0 ms (100%), Space: 7.6 MB (55.97%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-    int func(int lastCopy, int leng,int pasteEnable, int n)
-    {
-        if(leng == n) return 0;
-
-        int copyPaste,paste;
-        copyPaste=paste=INT_MAX/2;
-        
-        if(leng+leng<=n)
-        copyPaste=2+func(leng,leng+leng,1,n); 
-
-        if(pasteEnable==1 and leng+lastCopy<=n)
-        paste=1+func(lastCopy, leng+lastCopy,1, n);
-
-        return min(copyPaste,paste);
-    }
     int minSteps(int n) {
-        if(n==1) return 0;
-        return func(1,1,0,n);
+        if (n == 1) return 0;
+        
+        int steps = 0;
+        int factor = 2;
+        
+        while (n > 1) {
+            while (n % factor == 0) {
+                steps += factor;
+                n /= factor;
+            }
+            factor++;
+        }
+        
+        return steps;
     }
 };
